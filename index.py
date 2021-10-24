4e4d4428e63c09a16388ee115a68610229bdd016
hdeza <hdeza989@gmail.com>
2021-10-23 20:56:39 -0500

    funcion2

--- index.py
+++ index.py
@@ -22,8 +22,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)     
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datebase/metropolis.db"      
-login_manager = LoginManager(app)       
-login_manager.login_view = "login"      
+   
 db = SQLAlchemy(app)    
 
 
---
+++ static/js/funcion2/img.js
@@ -0,0 +1,4 @@
+function enviarImg() {
+    var image = document.getElementById("funcion")
+    image.src = "/static/images/PaginaPrincipal/peli4.jpg"
+}
