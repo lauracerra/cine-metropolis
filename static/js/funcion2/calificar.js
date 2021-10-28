316f5eb2ed03d8ac0568c7385476fdaf8c3a2d08
Angelica Romero <dallanaromero150@gmail.com>
2021-10-28 10:48:45 -0500

    calificar

--- static/js/funcion2/calificar.js
+++ static/js/funcion2/calificar.js
@@ -1,13 +1,3 @@
-var contador;
-function calificar(item){
-    console.log(item);
-    contador=item.id[0];
-    let nombre = item.id.substring(1);
-    for(let i=0;i<5;i++){
-        if (i<contador){
-            document.getElementById((i+1)+nombre).style.color="orange";
-        }
-    } 
-}
+
    
     
